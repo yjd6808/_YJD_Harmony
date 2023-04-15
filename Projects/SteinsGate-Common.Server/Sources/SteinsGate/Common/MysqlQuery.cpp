d33af7671ff9c82cf5995f8aebdd63d0a751838c
jdyun <wjdeh313@naver.com>
2023-04-15 20:25:33 +0900

    - MysqlQuery 세분화하여 성능개선

--- Projects/SteinsGate-Common.Server/Sources/SteinsGate/Common/MysqlQuery.cpp
+++ Projects/SteinsGate-Common.Server/Sources/SteinsGate/Common/MysqlQuery.cpp
@@ -5,17 +5,16 @@ USING_NS_JC;
 USING_NS_JC;
 USING_NS_STD;
 
-MysqlQuery::MysqlQuery(MysqlConnection *mConn, const JCore::String& preparedStatement) {
-	m_sqlConn = mConn;
-	m_PreparedStatement = preparedStatement;
-	m_bSuccess = false;
-}
+MysqlQuery::MysqlQuery(MysqlConnection* conn, const String& preparedStatement, StatementType type)
+	: m_pConn(conn)
+	, m_PreparedStatement(preparedStatement)
+	, m_bSuccess(false)
+	, m_eType(type)
+{}
 
-MysqlQuery::~MysqlQuery() {}
-
-StatementType MysqlQuery::ParseStatement(const JCore::String& statement) {
+StatementType MysqlQuery::ParseStatement(const String& statement) {
 	StatementType eStatement = StatementType::None;
-	const JCore::String statementPrefix = statement.GetRange(0, 5).ToLowerCase();
+	const String statementPrefix = statement.GetRange(0, StatementPrefixLength - 1).ToLowerCase();
 
 	// 각 스테이트먼트가 각 문자열로 시작하는지 확인
 	if (statementPrefix.Find("select") == 0) {
@@ -32,16 +31,42 @@ StatementType MysqlQuery::ParseStatement(const JCore::String& statement) {
 }
 
 
-JCore::String MysqlQuery::GetFieldName(const unsigned int& field) {
-	if (m_FieldMap.Size() < field) {
-		_LogError_("%d개의 필드밖에 없습니다.", m_FieldMap.Size());
+bool MysqlQueryUpdate::Execute() {
+	if (mysql_query(m_pConn->GetConnection(), m_PreparedStatement.Source())) {
+		const String erstr = m_pConn->GetLastError();
+		if (erstr.Length() > 2)
+			_LogError_("MySQL 오류 : %s", m_pConn->GetLastError().Source());
+		return false;
+	}
+
+	return m_bSuccess = true;
+}
+
+bool MysqlQueryInsert::Execute() {
+	if (mysql_query(m_pConn->GetConnection(), m_PreparedStatement.Source())) {
+		const String erstr = m_pConn->GetLastError();
+		if (erstr.Length() > 2)
+			_LogError_("MySQL 오류 : %s", m_pConn->GetLastError().Source());
+		return false;
+	}
+
+	// https://dev.mysql.com/doc/c-api/5.7/en/mysql-insert-id.html
+	m_uiInsertId = mysql_insert_id(m_pConn->GetConnection());
+	return true;
+}
+
+
+
+String MysqlQuerySelect::GetFieldName(const unsigned int& field) {
+	if (m_hFieldMap.Size() < field) {
+		_LogError_("%d개의 필드밖에 없습니다.", m_hFieldMap.Size());
 		return NULL;
 	}
 
-	return m_FieldMap[field];
+	return m_hFieldMap[field];
 }
 
-JCore::String MysqlQuery::GetString(const unsigned int& rowIdx, const unsigned int& fieldIdx) {
+String MysqlQuerySelect::GetString(const unsigned int& rowIdx, const unsigned int& fieldIdx) {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
 		return NULL;
@@ -52,13 +77,13 @@ JCore::String MysqlQuery::GetString(const unsigned int& rowIdx, const unsigned i
 		return NULL;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
-	JCore::String sValue = rSelectedRow[fieldIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
+	String sValue = rSelectedRow[fieldIdx];
 
 	return sValue;
 }
 
-JCore::String MysqlQuery::GetString(const unsigned int& rowIdx, const JCore::String& fieldName) {
+String MysqlQuerySelect::GetString(const unsigned int& rowIdx, const String& fieldName) {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
 		return NULL;
@@ -69,14 +94,14 @@ JCore::String MysqlQuery::GetString(const unsigned int& rowIdx, const JCore::Str
 		return NULL;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
-	const int iFieldID = m_FieldStringToIntMap[fieldName];
-	JCore::String sValue = rSelectedRow[iFieldID];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
+	const int iFieldID = m_hFieldStringToIntMap[fieldName];
+	String sValue = rSelectedRow[iFieldID];
 
 	return sValue;
 }
 
-int MysqlQuery::GetInt(const unsigned int &rowIdx, const unsigned int &fieldIdx)
+int MysqlQuerySelect::GetInt(const unsigned int& rowIdx, const unsigned int& fieldIdx)
 {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
@@ -88,13 +113,13 @@ int MysqlQuery::GetInt(const unsigned int &rowIdx, const unsigned int &fieldIdx)
 		return NULL;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
 	const int iValue = atoi(rSelectedRow[fieldIdx].Source());
 
 	return iValue;
 }
 
-int MysqlQuery::GetInt(const unsigned int &rowIdx, const JCore::String &fieldName)
+int MysqlQuerySelect::GetInt(const unsigned int& rowIdx, const String& fieldName)
 {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
@@ -105,14 +130,14 @@ int MysqlQuery::GetInt(const unsigned int &rowIdx, const JCore::String &fieldNam
 		_LogError_("%d개의 쿼리 결과만 존재합니다. 입력하신 행 인덱스값이 %d입니다.", GetResultRowCount(), rowIdx);
 		return NULL;
 	}
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
 
-	const int iFieldID = m_FieldStringToIntMap[fieldName];
+	const int iFieldID = m_hFieldStringToIntMap[fieldName];
 	const int iValue = atoi(rSelectedRow[iFieldID].Source());
 	return iValue;
 }
 
-double MysqlQuery::GetDouble(const unsigned int &rowIdx, const unsigned int &fieldIdx) {
+double MysqlQuerySelect::GetDouble(const unsigned int& rowIdx, const unsigned int& fieldIdx) {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
 		return 0;
@@ -123,13 +148,13 @@ double MysqlQuery::GetDouble(const unsigned int &rowIdx, const unsigned int &fie
 		return 0;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
 	const double dValue = atof(rSelectedRow[fieldIdx].Source());
 
 	return dValue;
 }
 
-double MysqlQuery::GetDouble(const unsigned int &rowIdx, const JCore::String &fieldName) {
+double MysqlQuerySelect::GetDouble(const unsigned int& rowIdx, const String& fieldName) {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
 		return 0;
@@ -139,15 +164,15 @@ double MysqlQuery::GetDouble(const unsigned int &rowIdx, const JCore::String &fi
 		return 0;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
 
-	const int iFieldID = m_FieldStringToIntMap[fieldName];
+	const int iFieldID = m_hFieldStringToIntMap[fieldName];
 	const double dValue = atof(rSelectedRow[iFieldID].Source());
 
 	return dValue;
 }
 
-JCore::DateTime MysqlQuery::GetTime(const unsigned int &rowIdx, const unsigned int &fieldIdx) {
+DateTime MysqlQuerySelect::GetTime(const unsigned int& rowIdx, const unsigned int& fieldIdx) {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
 		return 0;
@@ -158,8 +183,8 @@ JCore::DateTime MysqlQuery::GetTime(const unsigned int &rowIdx, const unsigned i
 		return 0;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
-	const JCore::String& val = rSelectedRow[fieldIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
+	const String& val = rSelectedRow[fieldIdx];
 
 	int precision = atoi(val.GetRange(20, 5).Source());
 	const int mili = precision / 1000;
@@ -179,7 +204,7 @@ JCore::DateTime MysqlQuery::GetTime(const unsigned int &rowIdx, const unsigned i
 	).ToDateTime();
 }
 
-JCore::DateTime MysqlQuery::GetTime(const unsigned int &rowIdx, const JCore::String &fieldName) {
+DateTime MysqlQuerySelect::GetTime(const unsigned int& rowIdx, const String& fieldName) {
 	if (GetResultRowCount() < 1) {
 		_LogError_("쿼리 수행결과가 존재하지 않습니다");
 		return 0;
@@ -189,10 +214,10 @@ JCore::DateTime MysqlQuery::GetTime(const unsigned int &rowIdx, const JCore::Str
 		return 0;
 	}
 
-	TResultRow& rSelectedRow = m_ResultMap[rowIdx];
+	TResultRow& rSelectedRow = m_hResultMap[rowIdx];
 
-	const int iFieldID = m_FieldStringToIntMap[fieldName];
-	const JCore::String& val = rSelectedRow[iFieldID];
+	const int iFieldID = m_hFieldStringToIntMap[fieldName];
+	const String& val = rSelectedRow[iFieldID];
 	int precision = atoi(val.GetRange(20, 25).Source());
 	const int mili = precision / 1000;
 
@@ -212,46 +237,46 @@ JCore::DateTime MysqlQuery::GetTime(const unsigned int &rowIdx, const JCore::Str
 }
 
 
-unsigned int MysqlQuery::GetResultRowCount() const {
-	const int iRowCount = m_ResultMap.Size();
+unsigned int MysqlQuerySelect::GetResultRowCount() const {
+	const int iRowCount = m_hResultMap.Size();
 	return iRowCount;
 }
 
-unsigned int MysqlQuery::GetFieldCount() const {
-	const int iFieldCount = m_FieldMap.Size();
+unsigned int MysqlQuerySelect::GetFieldCount() const {
+	const int iFieldCount = m_hFieldMap.Size();
 	return iFieldCount;
 }
 
 
-bool MysqlQuery::ExecuteQuery() {
-	if (mysql_query(m_sqlConn->GetConnection(), m_PreparedStatement.Source())) {
-		const JCore::String erstr = m_sqlConn->GetLastError();
+bool MysqlQuerySelect::Execute() {
+	if (mysql_query(m_pConn->GetConnection(), m_PreparedStatement.Source())) {
+		const String erstr = m_pConn->GetLastError();
 		if (erstr.Length() > 2)
-			_LogError_("MySQL 오류 : %s", m_sqlConn->GetLastError().Source());
-		
+			_LogError_("MySQL 오류 : %s", m_pConn->GetLastError().Source());
+
 		return false;
 	}
 
-	MYSQL_RES *result = mysql_store_result(m_sqlConn->GetConnection());
+	MYSQL_RES* result = mysql_store_result(m_pConn->GetConnection());
 
 	if (result == NULL) {
-		const JCore::String erstr = m_sqlConn->GetLastError();
+		const String erstr = m_pConn->GetLastError();
 		if (erstr.Length() > 2)
-			_LogError_("MySQL 오류 : %s", m_sqlConn->GetLastError().Source());
+			_LogError_("MySQL 오류 : %s", m_pConn->GetLastError().Source());
 		return false;
 	}
 
 
 	const int iNumFields = mysql_num_fields(result);
 	MYSQL_ROW row;
-	MYSQL_FIELD *field;
+	MYSQL_FIELD* field;
 
 	// 필드 이름에서 필드 인덱스를 얻을 수 있도록 하고
 	// 필드 인덱스에서 필드 이름을 얻을 수 있도록 한다.
 	int i = 0;
 	while ((field = mysql_fetch_field(result))) {
-		m_FieldMap.Insert(i, field->name);
-		m_FieldStringToIntMap.Insert(field->name, i);
+		m_hFieldMap.Insert(i, field->name);
+		m_hFieldStringToIntMap.Insert(field->name, i);
 		i++;
 	}
 
@@ -262,51 +287,10 @@ bool MysqlQuery::ExecuteQuery() {
 		for (int n = 0; n < iNumFields; n++)
 			resRow.Insert(n, row[n] ? row[n] : "NULL");
 
-		m_ResultMap.Insert(i, resRow);
+		m_hResultMap.Insert(i, resRow);
 
 		i++;
 	}
 
-	
-	return m_bSuccess = true;
+	return m_hResultMap.Size();
 }
-
-bool MysqlQuery::ExecuteUpdate() {
-	if (mysql_query(m_sqlConn->GetConnection(), m_PreparedStatement.Source())) {
-		const JCore::String erstr = m_sqlConn->GetLastError();
-		if (erstr.Length() > 2)
-			_LogError_("MySQL 오류 : %s", m_sqlConn->GetLastError().Source());
-		return false;
-	}
-
-	return m_bSuccess = true;
-}
-
-Int64U MysqlQuery::ExecuteInsert() {
-	if (mysql_query(m_sqlConn->GetConnection(), m_PreparedStatement.Source())) {
-		const JCore::String erstr = m_sqlConn->GetLastError();
-		if (erstr.Length() > 2)
-			_LogError_("MySQL 오류 : %s", m_sqlConn->GetLastError().Source());
-		return 0;
-	}
-
-	const Int64U iLastInsertID = mysql_insert_id(m_sqlConn->GetConnection());
-
-	m_bSuccess = true;
-	return iLastInsertID;
-}
-
-bool MysqlQuery::ExecuteAuto() {
-	switch (ParseStatement(m_PreparedStatement)) {
-	case StatementType::Select:
-		return ExecuteQuery();
-	case StatementType::Insert:
-		return ExecuteInsert() != 0;
-	case StatementType::Update:
-	case StatementType::Delete:
-		return ExecuteUpdate();
-	default:
-		return ExecuteQuery();
-	}
-}
-
--- Projects/SteinsGate-Common.Server/Sources/SteinsGate/Common/MysqlQuery.h
+++ Projects/SteinsGate-Common.Server/Sources/SteinsGate/Common/MysqlQuery.h
@@ -20,12 +20,19 @@
 #include <SteinsGate/Common/StatementType.h>
 
 
+class MysqlQuery;
+class MysqlQuerySelect;
+class MysqlQueryUpdate;
+class MysqlQueryInsert;
+
+using MysqlQueryPtr = JCore::SharedPtr<MysqlQuery>;
 class MysqlQuery
 {
-	using TResultRow = JCore::HashMap<int, JCore::String>;
 public:
-	MysqlQuery(MysqlConnection *mConn, const JCore::String& preparedStatement);
-	~MysqlQuery();
+	static constexpr int StatementPrefixLength = JCore::StringUtil::CTLength("select");
+
+	MysqlQuery(MysqlConnection* conn, const JCore::String& preparedStatement, StatementType type);
+	virtual ~MysqlQuery() = default;
 
 	// 앞 6글자를 확인해서 어떤 타입의 Statement인지 확인
 	// 문자열이 select로 시작하면 SELECT 타입 - 뭐 이런식
@@ -33,50 +40,98 @@ public:
 
 	// 만들면서 동시에 인자를 넣어주도록...
 	template <typename... Args>
-	static MysqlQuery* Create(MysqlConnection* conn, const JCore::String& statement, Args&&... args) {
-		JCore::String preparedStatement = MysqlStatementBuilder::Build(statement, JCore::Forward<Args>(args)...);
+	static MysqlQueryPtr Create(MysqlConnection* conn, const JCore::String& statement, Args&&... args) {
+		const JCore::String preparedStatement = MysqlStatementBuilder::Build(statement, JCore::Forward<Args>(args)...);
 
 		if (preparedStatement == "")
 			return nullptr;
 
-		return new MysqlQuery(conn, preparedStatement);
+		MysqlQueryPtr spQuery;
+
+		switch (ParseStatement(preparedStatement)) {
+		case StatementType::Select: 
+			spQuery = JCore::MakeShared<MysqlQuerySelect>(conn, preparedStatement, StatementType::Select);
+			break;
+		case StatementType::Update:
+			spQuery = JCore::MakeShared<MysqlQueryUpdate>(conn, preparedStatement, StatementType::Update);
+			break;
+		case StatementType::Delete:
+			spQuery = JCore::MakeShared<MysqlQueryUpdate>(conn, preparedStatement, StatementType::Delete);
+			break;
+		case StatementType::Insert: 
+			spQuery = JCore::MakeShared<MysqlQueryInsert>(conn, preparedStatement, StatementType::Insert);
+			break;
+		default:
+			DebugAssertMsg(false, "올바르지 않은 스테이트먼트입니다.");
+		}
+
+
+		return spQuery;
 	}
 
-	
-	// SELECT
-	bool ExecuteQuery();
+	MysqlConnection* GetConnection() const { return m_pConn; }
+	virtual bool Execute() = 0;
+	virtual bool IsSuccess() const { return m_bSuccess; }
+	virtual bool IsFailed() const { return !m_bSuccess; }
+protected:
+	StatementType m_eType;
+	MysqlConnection* m_pConn;
+	JCore::String m_PreparedStatement;
+	bool m_bSuccess;
+};
 
-	// DELETE | UDDATE 용
-	bool ExecuteUpdate();
+class MysqlQueryUpdate : public MysqlQuery
+{
+public:
+	MysqlQueryUpdate(MysqlConnection* conn, const JCore::String& preparedStatement, StatementType type)
+		: MysqlQuery(conn, preparedStatement, type)
+	{}
+	~MysqlQueryUpdate() override = default;
 
-	// INSERT 실행후 마지막 ID값 얻음
-	Int64U ExecuteInsert();
+	bool Execute() override;
+};
 
-	// 스테이트먼트 앞 6글자를 파싱해서 select, delete 등에 맞게 알아서 실행
-	bool ExecuteAuto();
+class MysqlQueryInsert : public MysqlQuery
+{
+public:
+	MysqlQueryInsert(MysqlConnection* conn, const JCore::String& preparedStatement, StatementType type)
+		: MysqlQuery(conn, preparedStatement, type)
+		, m_uiInsertId(0)
+	{}
+	~MysqlQueryInsert() override = default;
+
+	bool Execute() override;
+	Int64U GetInsertId() const { return m_uiInsertId; }
+private:
+	Int64U m_uiInsertId;
+};
+
+class MysqlQuerySelect : public MysqlQuery
+{
+	using TResultRow = JCore::HashMap<int, JCore::String>;
+public:
+	MysqlQuerySelect(MysqlConnection* conn, const JCore::String& preparedStatement, StatementType type)
+		: MysqlQuery(conn, preparedStatement, type)
+	{}
+	~MysqlQuerySelect() override = default;
+
+	bool Execute() override;
 
 	JCore::String GetFieldName(const unsigned int& field);
 	JCore::String GetString(const unsigned int& rowIdx, const unsigned int& fieldIdx);
 	JCore::String GetString(const unsigned int& rowIdx, const JCore::String& fieldName);
-	int GetInt(const unsigned int &rowIdx, const unsigned int &fieldIdx);
-	int GetInt(const unsigned int &rowIdx, const JCore::String &fieldName);
-	double GetDouble(const unsigned int &rowIdx, const unsigned int &fieldIdx);
-	double GetDouble(const unsigned int &rowIdx, const JCore::String &fieldName);
-	JCore::DateTime GetTime(const unsigned int &rowIdx, const unsigned int &fieldIdx);
-	JCore::DateTime GetTime(const unsigned int &rowIdx, const JCore::String &fieldName);
+	int GetInt(const unsigned int& rowIdx, const unsigned int& fieldIdx);
+	int GetInt(const unsigned int& rowIdx, const JCore::String& fieldName);
+	double GetDouble(const unsigned int& rowIdx, const unsigned int& fieldIdx);
+	double GetDouble(const unsigned int& rowIdx, const JCore::String& fieldName);
+	JCore::DateTime GetTime(const unsigned int& rowIdx, const unsigned int& fieldIdx);
+	JCore::DateTime GetTime(const unsigned int& rowIdx, const JCore::String& fieldName);
 
 	unsigned int GetResultRowCount() const;
 	unsigned int GetFieldCount() const;
-
-	bool IsSuccess() const { return m_bSuccess; }
-	bool IsFailed() const { return !m_bSuccess; }
-
 private:
-	MysqlConnection* m_sqlConn;
-	JCore::String m_PreparedStatement;
-	bool m_bSuccess;
-	JCore::HashMap<int, TResultRow> m_ResultMap;
-	JCore::HashMap<int, JCore::String> m_FieldMap;
-	JCore::HashMap<JCore::String, int> m_FieldStringToIntMap;
+	JCore::HashMap<int, TResultRow> m_hResultMap;
+	JCore::HashMap<int, JCore::String> m_hFieldMap;
+	JCore::HashMap<JCore::String, int> m_hFieldStringToIntMap;
 };
 
