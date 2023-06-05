90993694e507fd372b83b068b728871e620ac4f2
jdyun <wjdeh313@naver.com>
2023-06-06 00:36:46 +0900

    - IsCallable, CallableSignature 테스트 추가

--- Projects/JCore-Test/Sources/JCoreTest/TypeTraitsTest.cpp
+++ Projects/JCore-Test/Sources/JCoreTest/TypeTraitsTest.cpp
@@ -115,4 +115,100 @@ TEST(TypeTraitsTest, IsValidAllocator) {
 	EXPECT_TRUE(b);
 }
 
+static int* globalFn(char, int*, double&) {}
+struct op
+{
+	void operator()() {}
+	void memberFn() {}
+	static void staticFn() {}
+};
+TEST(TypeTraitsTest, IsCallable) {
+	EXPECT_TRUE(IsCallable_v<decltype([] {})>);
+	EXPECT_TRUE(IsCallable_v<op>);
+	EXPECT_TRUE(IsCallable_v<decltype(&op::memberFn)>);
+	EXPECT_TRUE(IsCallable_v<decltype(&op::staticFn)>);
+	EXPECT_TRUE(IsCallable_v<decltype(op::staticFn)>);
+	EXPECT_TRUE(IsCallable_v<decltype(globalFn)>);
+	EXPECT_TRUE(IsCallable_v<decltype(&globalFn)>);
+}
+
+static void globalFn1(int) {}
+static bool globalFn2(int*, double*) { return true; }
+struct t2
+{
+	struct op1 { void operator()(int) {} };
+	struct op2 { bool operator()(int*, double*) { return true; } };
+	void memberFn1(int) {}
+	bool memberFn2(int*, double*) { return true; }
+	static void staticFn1(int) {}
+	static bool staticFn2(int*, double*) { return true; }
+};
+
+TEST(TypeTraitsTest, CallableSignature) {
+	auto lambda1 = [](int) {};
+	auto lambda2 = [](int*, double*)->bool { return true; };
+
+	constexpr bool _1 = IsSameType_v<CallableSignature_t<decltype(lambda1)>::Parameters, ParameterPack_t<int>>;
+	constexpr bool _2 = IsSameType_v<CallableSignature_t<decltype(lambda1)>::Return, void>;
+
+	constexpr bool _3 = IsSameType_v<CallableSignature_t<decltype(lambda2)>::Parameters, ParameterPack_t<int*, double*>>;
+	constexpr bool _4 = IsSameType_v<CallableSignature_t<decltype(lambda2)>::Return, bool>;
+
+	constexpr bool _5 = IsSameType_v<CallableSignature_t<decltype(globalFn1)>::Parameters, ParameterPack_t<int>>;
+	constexpr bool _6 = IsSameType_v<CallableSignature_t<decltype(globalFn1)>::Return, void>;
+
+	constexpr bool _7 = IsSameType_v<CallableSignature_t<decltype(&globalFn1)>::Parameters, ParameterPack_t<int>>;
+	constexpr bool _8 = IsSameType_v<CallableSignature_t<decltype(&globalFn1)>::Return, void>;
+
+	constexpr bool _9 = IsSameType_v<CallableSignature_t<decltype(globalFn2)>::Parameters, ParameterPack_t<int*, double*>>;
+	constexpr bool _10 = IsSameType_v<CallableSignature_t<decltype(globalFn2)>::Return, bool>;
+
+	constexpr bool _11 = IsSameType_v<CallableSignature_t<decltype(&globalFn2)>::Parameters, ParameterPack_t<int*, double*>>;
+	constexpr bool _12 = IsSameType_v<CallableSignature_t<decltype(&globalFn2)>::Return, bool>;
+
+	constexpr bool _13 = IsSameType_v<CallableSignature_t<decltype(&t2::memberFn1)>::Parameters, ParameterPack_t<int>>;
+	constexpr bool _14 = IsSameType_v<CallableSignature_t<decltype(&t2::memberFn1)>::Return, void>;
+
+	constexpr bool _15 = IsSameType_v<CallableSignature_t<decltype(&t2::memberFn2)>::Parameters, ParameterPack_t<int*, double*>>;
+	constexpr bool _16 = IsSameType_v<CallableSignature_t<decltype(&t2::memberFn2)>::Return, bool>;
+
+	constexpr bool _17 = IsSameType_v<CallableSignature_t<decltype(t2::staticFn1)>::Parameters, ParameterPack_t<int>>;
+	constexpr bool _18 = IsSameType_v<CallableSignature_t<decltype(t2::staticFn1)>::Return, void>;
+
+	constexpr bool _19 = IsSameType_v<CallableSignature_t<decltype(&t2::staticFn1)>::Parameters, ParameterPack_t<int>>;
+	constexpr bool _20 = IsSameType_v<CallableSignature_t<decltype(&t2::staticFn1)>::Return, void>;
+
+	constexpr bool _21 = IsSameType_v<CallableSignature_t<decltype(t2::staticFn2)>::Parameters, ParameterPack_t<int*, double*>>;
+	constexpr bool _22 = IsSameType_v<CallableSignature_t<decltype(t2::staticFn2)>::Return, bool>;
+
+	constexpr bool _23 = IsSameType_v<CallableSignature_t<decltype(&t2::staticFn2)>::Parameters, ParameterPack_t<int*, double*>>;
+	constexpr bool _24 = IsSameType_v<CallableSignature_t<decltype(&t2::staticFn2)>::Return, bool>;
+
+
+	EXPECT_TRUE(_1);
+	EXPECT_TRUE(_2);
+	EXPECT_TRUE(_3);
+	EXPECT_TRUE(_4);
+	EXPECT_TRUE(_5);
+	EXPECT_TRUE(_6);
+	EXPECT_TRUE(_7);
+	EXPECT_TRUE(_8);
+	EXPECT_TRUE(_9);
+	EXPECT_TRUE(_10);
+	EXPECT_TRUE(_11);
+	EXPECT_TRUE(_12);
+	EXPECT_TRUE(_13);
+	EXPECT_TRUE(_14);
+	EXPECT_TRUE(_15);
+	EXPECT_TRUE(_16);
+	EXPECT_TRUE(_17);
+	EXPECT_TRUE(_18);
+	EXPECT_TRUE(_19);
+	EXPECT_TRUE(_20);
+	EXPECT_TRUE(_21);
+	EXPECT_TRUE(_22);
+	EXPECT_TRUE(_23);
+	EXPECT_TRUE(_24);
+}
+
 #endif // TEST_TypeTraitsTest == ON
