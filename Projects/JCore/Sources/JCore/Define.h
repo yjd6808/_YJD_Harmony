4efb945f4bc3e5f8332c291d2532f89086e40763
jdyun <wjdeh313@naver.com>
2023-08-08 06:49:24 +0900

    - Cocos2d-x에서 자주 사용하던 바인딩 디파인 라이브러리에 추가

--- Projects/JCore/Sources/JCore/Define.h
+++ Projects/JCore/Sources/JCore/Define.h
@@ -124,4 +124,11 @@
 	
 	#define JCORE_SWAP(val1, val2, type) do { type temp = val1; val1 = val2; val2 = temp; } while (0)
 
+	// Cocos2d-x에서 자주사용하던 바인딩
+	#define JCORE_CALLBACK_0(__selector__,__target__, ...) std::bind(&__selector__,__target__, ##__VA_ARGS__)
+	#define JCORE_CALLBACK_1(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, ##__VA_ARGS__)
+	#define JCORE_CALLBACK_2(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, std::placeholders::_2, ##__VA_ARGS__)
+	#define JCORE_CALLBACK_3(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, ##__VA_ARGS__)
+
+
 #endif
